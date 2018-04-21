a7f29fcc29801729b570917c1f6467a32ebdce3e
hzs90 <zsolt.huszar@live.com>
2018-04-21 18:23:56 +0200

    add remove actions

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -5,28 +5,107 @@ console.log('Starting todo redux example');
 var stateDefault = {
   searchText: '',
   showCompleted: false,
-  todos: []
+  todos: [],
+  hobbies: [],
+  movies: []
 };
 
+var nextHobbyId = 1;
+var nextMovieId = 1;
 var reducer = (state = stateDefault, action) => {
   switch(action.type){
     case 'CHANGE_SEARCH_TEXT':
       return {
         ...state,
         searchText: action.searchText
-      }
+      };
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+        }
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+    };
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: nextMovieId++,
+            movie: action.movie,
+            genre: action.genre
+          }
+        ]
+    };
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie.id !== action.id)
+        }
     default:
       return state;
   }
 };
 
-var store = redux.createStore(reducer);
-console.log('currentState', store.getState());
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
+//subscribe to changes
+store.subscribe(() => {
+  var state = store.getState();
+  console.log('searchText is', state.searchText);
+  document.getElementById('app').innerHTML = state.searchText;
+});
 
 store.dispatch({
   type: 'CHANGE_SEARCH_TEXT',
   searchText: 'work'
 });
 
-console.log('searcHText should be work', store.getState());
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'play'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  movie: 'Spiderman',
+  genre: 'Action'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  movie: 'Moon',
+  genre: 'Scifi'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 2
+});
